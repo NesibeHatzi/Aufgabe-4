36b0a8a9f107c71559674474a33e98a513526c48
NesibeHatzi <37589455+NesibeHatzi@users.noreply.github.com>
2018-04-29 17:03:54 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,12 +10,24 @@ namespace Aufgabe4_GenerischerBaum
     {
         static void Main(string[] args)
         {
-    var tree = new Tree<String>();
-    var root = tree.CreateNode("root");
-    var child1 = tree.CreateNode("child1");
-    var child2 = tree.CreateNode("child1");
-    root.AppendChild(child1);
-    root.AppendChild(child2);
+    
+            var tree = new Tree<String>();
+            var root = tree.CreateNode("root");
+            var child1 = tree.CreateNode("child1");
+            var child2 = tree.CreateNode("child1");
+            root.AppendChild(child1);
+            root.AppendChild(child2);
+            var grand11 = tree.CreateNode("grand11");
+            var grand12 = tree.CreateNode("grand12");
+            var grand13 = tree.CreateNode("grand13");
+            child1.AppendChild(grand11);
+            child1.AppendChild(grand12);
+            child1.AppendChild(grand13);
+            var grand21 = tree.CreateNode("grand21");
+            child2.AppendChild(grand21);
+            child1.RemoveChild(grand12);
+
+root.PrintTree(); 
         }
     }
 
